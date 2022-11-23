42aba972a6a6a959d3e608f6efa29da6ab30bad2
lucastozo <lucastozo7@gmail.com>
2022-11-23 11:13:19 -0400

    TRABALHO

---
+++ erro404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Página não encontrada!</title>
+</head>
+
+<body>
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div>
+            <img src="Images/monkey-thinking.jpg" class="rounded">
+        </div>
+        <div class="text-center">
+            <h1>Página não encontrada!</h1>
+            <p>A página que você tentou acessar não existe.</p>
+        </div>
+        <div class="text-center">
+            <a class="btn btn-primary" href="index.php" role="button">Ir para a página inicial</a>
+        </div>
+    </div>
+
+    <?php include '_parts/_linkJS.php'; ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Usuários</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <?php include_once '_parts/_header.php'; ?>
+    <div class="p-5 text-center bg-light" style="margin-top: 58px;">
+
+    </div>
+    <?php include '_parts/_linkJS.php'; ?>
+
+    <div class="position-relative">
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Vídeo final</h5>
+                        <p class="card-text">Clique aqui para assistir ao vídeo final do trabalho Interdisciplinar.</p>
+                        <a href="#" target="_blank" class="btn btn-primary">Assistir Vídeo</a>
+                    </div>
+                </div>
+                </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Arquivos do trabalho</h5>
+                            <p class="card-text">Clique aqui para acessar os arquivos relacionados ao trabalho Interdisciplinar.</p>
+                            <a href="https://drive.google.com/drive/folders/16DZgm_m6ChRi_ZjCU9aeRPFpk13-AUFs" target="_blank" class="btn btn-primary">Google Drive</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
